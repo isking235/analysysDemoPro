bc15e98b7f966b2497f4767e2a67cb3a8570ec4a
sangho235 <sangho235@it.gsshop.com>
2022-04-17 09:41:08 +0900

    CSR001 최신 주가 의견부터 조회해 저장한다.

--- crawlerEventOpinion.js
+++ crawlerEventOpinion.js
@@ -1,4 +1,9 @@
 /*
+history
+2022-04-16 이상호 [CSR001]_종목별 최신을 읽어와 이후 종목 의견만 받아온다.
+ */
+
+/*
 종목 정보 테이블을 읽어서 목표가및 의견 정보를 입력하자
  */
 const axios = require('axios');
@@ -14,40 +19,97 @@ const conn = {  // mysql 접속 설정
     database: process.env.DB_NAME,
 };
 
+/*
+2022-04-16 날짜를 datetime 포멧으로 가져온다.
+**/
+function datetimeFormat(date) {
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+
+    month = month >= 10 ? month : '0' + month;
+    day = day >= 10 ? day : '0' + day;
+    hour = hour >= 10 ? hour : '0' + hour;
+    minute = minute >= 10 ? minute : '0' + minute;
+    second = second >= 10 ? second : '0' + second;
+
+    return date.getFullYear() + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+}
+
+/*
+2022-04-16 날짜를 date 포멧으로 가져온다.
+**/
+function dateFormat(date) {
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+
+    month = month >= 10 ? month : '0' + month;
+    day = day >= 10 ? day : '0' + day;
+    hour = hour >= 10 ? hour : '0' + hour;
+    minute = minute >= 10 ? minute : '0' + minute;
+    second = second >= 10 ? second : '0' + second;
+
+    return date.getFullYear() + '/' + month + '/' + day;
+}
+
 const opinionLoad = async(eventCode) => {
 
     let opinionUrl = `http://m.comp.fnguide.com/m2/data/json/chart/01_02/chart_A${eventCode}.json`;
     console.log("opinionLoad.opinionUrl :"+ opinionUrl);
 
+
+
     const response = await axios.get(opinionUrl);
 
     if (response.status === 200) {
+
         const opnions = JSON.stringify(response.data, null, 2); //API를 json으로 받아 온다.
         //console.log(opnions);
         const obj = JSON.parse(opnions); //json을 객체화
         //console.log(obj.CHART);
         const list = obj.CHART;
 
-        var connection = mysql.createConnection(conn); // DB 커넥션 생성
+        let connection = mysql.createConnection(conn); // DB 커넥션 생성
         connection.connect();   // DB 접속
 
-        for(key in list) {
-            //console.log(list[key].TRD_DT);
-            testQuery = `INSERT INTO stock.opinion_target_price (reg_dtm,regr_id,mod_dtm,modr_id,event_code, opinion_date, investment_opinion, target_price, revised_stock_price) 
-            VALUES (NOW(),'LSH',NOW(),'LSH','${eventCode}','${list[key].TRD_DT}','${list[key].VAL1}','${list[key].VAL2}','${list[key].VAL3}');`;
-            
-            connection.query(testQuery, function (err, results, fields) { // testQuery 실행
-                if (err) {
-                    console.log(err);
+        /*[CSR001]_종목별 최신을 읽어와 이후 종목 의견만 받아온다*/
+        let maxOpinionDate = '';
+        let maxOpinionDateQuery = `SELECT MAX(A.opinion_date) AS max_opinion_date FROM opinion_target_price A WHERE A.event_code = '${eventCode}'`;
+
+        connection.query(maxOpinionDateQuery, function(err, results, field) {
+            if (err) {
+                console.log(err);
+            }
+
+            for(key in results) {
+                maxOpinionDate = dateFormat(results[key].max_opinion_date);
+            }
+
+            let insertQuery = '';
+
+            for(key in list) {
+                if(list[key].TRD_DT > maxOpinionDate) {
+                    //console.log(list[key].TRD_DT);
+                    insertQuery = `INSERT INTO stock.opinion_target_price (reg_dtm,regr_id,mod_dtm,modr_id,event_code, opinion_date, investment_opinion, target_price, revised_stock_price)
+                                    VALUES (NOW(),'LSH',NOW(),'LSH','${eventCode}','${list[key].TRD_DT}','${list[key].VAL1}','${list[key].VAL2}','${list[key].VAL3}');`;
+
+                    connection.query(insertQuery, function (err, results, fields) { // insertQuery 실행
+                        if (err) {
+                            console.log(err);
+                        }
+                    });
                 }
-            });
-        }
+            }
+            console.log("종목 :"+eventCode+" 입력 완료");
+            connection.end();
 
-        console.log("종목:"+eventCode)+"완료";
-        connection.end();
-
+        });
     }
-    
 };
 
 const runFunction = (resolve,eventCode,t) => {
@@ -64,12 +126,12 @@ function sleep(eventCode, t){
 const crawlerEventOpinion  = () => {
     console.log("crawlerEventOpinion start");
     /*DB에 입력 해 보자*/
-    var connection = mysql.createConnection(conn); // DB 커넥션 생성
+    let connection = mysql.createConnection(conn); // DB 커넥션 생성
     connection.connect();   // DB 접속
 
     /*쿼리 생성 한다.*/
-    //var testQuery = "SELECT event_code, company_name FROM event_info WHERE event_code in ('270870','067990','033500','141000');";
-    var testQuery = "SELECT event_code, company_name FROM event_info ORDER BY event_code;";
+    //let testQuery = "SELECT event_code, company_name FROM event_info WHERE event_code in ('270870','067990','033500','141000');";
+    let testQuery = "SELECT event_code, company_name FROM event_info WHERE event_code ORDER BY event_code";
     let intever = 2000;
     let ms = 0;
     let idx = 0;
