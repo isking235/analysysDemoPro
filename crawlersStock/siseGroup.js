c8e2f888322423ce7f0d814b602a277615b99aaf
DESKTOP-3M3CUSF\lsh <isking235@hanmail.net>
2025-04-12 01:56:45 +0900

    시세 그룹 타입을 크롤링 한다.

--- crawlersStock/siseGroup.js
+++ crawlersStock/siseGroup.js
@@ -3,9 +3,14 @@ const cheerio = require('cheerio');
 const https = require('https');
 const iconv = require('iconv-lite');
 
+const createLogger = require('../config/logger'); // config/logger.js에서 로거 가져
+const {scheduleStockSiseType, saveSiseGroup} = require('../utils/siseTypeUtil'); 
+
 const agent = new https.Agent({
       rejectUnauthorized: false
     });
+
+const logger = createLogger(__filename);    
 /*
     
     1. 페이지를 loof를 수행 하며 그룹 목록을 조회 
@@ -13,22 +18,34 @@ const agent = new https.Agent({
     */
 
 async function saveGroupList(page) {
-  const url = 'https://finance.naver.com/sise/sise_group.naver?type=group';
-  console.log(url);
+  logger.info(`saveGroupList()`);
+  const url = process.env.SISE_GROUP_URL;//https://finance.naver.com/sise/sise_group.naver?type=group
+  logger.info(`Target URL: ${url}`);
+
   const response = await axios.get(url, { responseType: 'arraybuffer', httpsAgent: agent });
   const content = iconv.decode(response.data, 'EUC-KR');
   const $ = cheerio.load(content);
+
+  //크롤링 대상 테이블 지정 및 순회 하면서 저장
   const groupTable  = $('.type_1');
+
     groupTable.find('a').each((index,element) => {
-      // 각 <a> 태그에 대한 작업 수행
+      
       const aTag = $(element); // Cheerio 객체로 변환
       const groupName = aTag.text(); // 그룹 이름 가져오기
       const groupLink = aTag.attr('href'); // 그룹 링크 가져오기
-       if (groupLink && groupLink.includes('type=group')) {
-        
+
+      if (groupLink && groupLink.includes('type=group')) {
         const noMatch = groupLink.match(/no=(\d+)/);
-        const noMatchNumber = noMatch ? noMatch[1] : null;
-        console.log(`Group Name: ${groupName}, Link: ${groupLink}, noMatchNumber: ${noMatchNumber}`);
+        const siseGrpDtlNo = noMatch ? noMatch[1] : null;
+
+        logger.debug(`Group Name: ${groupName}, Link: ${groupLink}, siseGrpDtlNo: ${siseGrpDtlNo}`);
+
+        //시세 타입에 해당 하는 종목을 입력 한다.
+        scheduleStockSiseType(logger, groupLink, 'group', siseGrpDtlNo, index * 1000);
+
+        //시세 타입을 시세타입기준테이블에 입력한다.
+        saveSiseGroup(logger,'group', siseGrpDtlNo, groupName);
        }
       
   });
